3cc0ace474033a9c4c5f492260c43cc67a0c5678
Dwain Madolid <dwainmadolid@hotmail.co.uk>
2024-05-07 13:58:21 +0100

    styled navbar

--- components/header.jsx
+++ components/header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <nav>
+      <nav className='navbar'>
       <Link to="/" className="navbar-item">
           Home
         </Link>

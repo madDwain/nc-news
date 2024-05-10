ea33abe4fbbbf082d6618ca158bd475ccb74e1b0
Dwain Madolid <dwainmadolid@hotmail.co.uk>
2024-05-10 14:17:06 +0100

    fixed vote buttonns

--- src/components/singleComment.jsx
+++ src/components/singleComment.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useLogin } from "../contexts/login";
 
 const SingleComment = ({ comment }) => {
+  const { user } = useLogin();
   return (
     <div className="single-comment-container">
       <div className="single-article-line">
@@ -10,6 +12,12 @@ const SingleComment = ({ comment }) => {
       <p className="comment-body">{comment.body}</p>
       <div className="single-article-line">
         <p className="article-vote-count">Vote count: {comment.votes}</p>
+        <button
+          className="btn"
+          id={`user-comment-${user !== null ? true : false}`}
+        >
+          Delete Comment
+        </button>
       </div>
     </div>
   );
